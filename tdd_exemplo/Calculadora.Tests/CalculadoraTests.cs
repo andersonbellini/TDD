83a2fcd063f34eecb5ded63c69a5cc084dbb9bcb
anderson.bellini <anderson.bellini@anac.gov.br>
2018-12-10 11:34:13 -0200

    Criação

---
+++ tdd_exemplo/Calculadora.Tests/CalculadoraTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+
+namespace Calculadora.Tests
+{
+    public class CalculadoraTests
+    {       
+        [TestFixture]
+        public class CalculadoraSimplesTests {
+
+            [Test]
+            public void DeveAdicionarDoisNumeros()
+            {
+                var sut = new CalculadoraSimples();
+                var resultado = sut.Adicionar(5, 5);
+                Assert.That(resultado, Is.EqualTo(10));
+
+            }
+
+            [Test]
+            public void DeveMultiplicarDoisNumeros()
+            {
+                var sut = new CalculadoraSimples();
+                var resultado = sut.Multiplicar(3, 5);
+                Assert.That(resultado, Is.EqualTo(15));
+
+            }
+
+            [Test]
+            public void DeveDividirDoisNumeros()
+            {
+                var sut = new CalculadoraSimples();
+                var resultado = sut.Dividir(25, 5);
+                Assert.That(resultado, Is.EqualTo(5));
+            }
+
+            [Test]
+            public void DeveDiminuirDoisNumeros()
+            {
+                var sut = new CalculadoraSimples();
+                var resultado = sut.Diminuir(25, 5);
+                Assert.That(resultado, Is.EqualTo(20));
+            }
+
+        }
+
+    }
+}
---
+++ tdd_exemplo/Calculadora/CalculadoraSimples.cs
@@ -0,0 +1,29 @@
+﻿namespace Calculadora
+{
+    public class CalculadoraSimples
+    {
+        //Método para add 2 numeros
+        public int Adicionar(int num1, int num2) {
+            return num1 + num2;
+        }
+
+        //Método para multiplicar
+        public int Multiplicar(int num1, int num2) {
+            return num1 * num2;
+        }
+
+        //Método para dividir 2 numeros
+        public int Dividir(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+        
+
+        //Método para diminuir 2 numeros
+        public int Diminuir(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+    }
+}
